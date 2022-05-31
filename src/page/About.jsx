adeee1f1a1c905a59c30b70f88e70a3fc1b9b5d9
hjseong <hjseong1222@gmail.com>
2022-05-31 12:25:20 +0900

    router-2

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
     <div>
       <h1>소개</h1>
       <p>소개 화면입니다</p>
+      <Link to="/">홈</Link>
     </div>
   );
 };
